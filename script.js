3293365b29b9ef332ca67bee30908b2c20297223
Frank GP <fgp555@gmail.com>
2023-01-27 14:50:25 -0500

    03 app redirect

---
+++ script.js
@@ -0,0 +1,15 @@
+const routes = {
+  404: "/404-page.html",
+  "/": "/home.html",
+  "/google": "https://google.com/",
+  "/example": "https://example.com/",
+};
+
+const handleLocation = () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  location.replace(route);
+};
+
+setTimeout(handleLocation, 0);
+// handleLocation();
