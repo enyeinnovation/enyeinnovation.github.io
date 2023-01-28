3c90d570a2a1e04bf4a868dd6ed7695011ece927
Frank GP <fgp555@gmail.com>
2023-01-28 09:32:02 -0500

    07 redirect v4

--- fgp-redirect-4.js
+++ fgp-redirect-4.js
@@ -9,17 +9,16 @@ style_redirect.innerHTML = /*css*/ `
       }
       .fgp-redirect a {
         color: inherit;
-        /* text-decoration: none; */
         display: block;
         padding: 0.3em;
       }
       .fgp-redirect strong {
         font-size: 1.5em;
+        color: khaki;
       }
       .fgp-redirect span 
       {
-        /* background-color: #fff; */
-        /* color: #000; */
+        color: coral;
         font-size: 3em;
       }
 
@@ -30,33 +29,24 @@ document.head.appendChild(style_redirect);
 let fgp_redirect = document.querySelectorAll(".fgp-redirect a");
 
 let path = window.location.pathname;
-// console.log(path)
 
 for (element of fgp_redirect) {
-  if (!element.textContent) element.textContent = element.href;
-
-  let domain = new URL(element.href);
-  // console.log(domain.pathname);
-
-  const myInterval = setInterval(function2, 5000);
-
-  function function2() {
-    if (domain.pathname == path) {
+  if (!element.textContent) element.textContent = window.location.hostname + element.id;
+  if (element.id == path) {
+    let asdf = element.href;
+    const myInterval = setInterval(function2, 5000);
+    function function2() {
+      console.log(asdf);
       clearInterval(myInterval);
-      // console.log('----------------')
-      console.log(element.id);
-      // function3(element);
-      // location.replace(element.id);
-      // window.location.id = element.id;
-
-      document.querySelector(".fgp-redirect h2").innerHTML = path;
-      document.querySelector(".fgp-redirect strong").innerHTML = element.id;
     }
+
+    document.querySelector(".fgp-redirect h2").innerHTML = path;
+    document.querySelector(".fgp-redirect strong").innerHTML = element.href;
   }
 }
 
+// ========== timer... ==========
 let span = document.querySelector(".fgp-redirect span");
-// console.log(span);
 var timeLeft = span.textContent;
 
 let interval = setInterval(function5, 1e3);
@@ -64,9 +54,7 @@ function function5() {
   timeLeft -= 1;
   span.innerHTML = timeLeft;
   timeLeft <= 0 && (clearInterval(interval), function3(), setTimeout(1));
-  // timeLeft <= 0 && (console.log(timeLeft))
   function function3(text) {
-    // console.log(text);
     document.querySelector(".message").innerHTML = "pagina no encontrada";
   }
 }
